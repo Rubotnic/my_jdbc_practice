388e10372fb0968aebbf28125d0a75e4804781d6
Rubotnic <101702711+Rubotnic@users.noreply.github.com>
2022-09-02 09:33:55 +0200

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,16 +8,6 @@ public class App {
     public static void main(String[] args) {
 
 
-        HashSet<String> customer = new LinkedHashSet<>();
-
-        customer.add("Kalle");
-        customer.add("Stefan");
-        customer.add("Marre");
-
-        Iterator<String> iter = customer.iterator();
-        while (iter.hasNext()){
-            System.out.println(iter.hasNext()+ "");
-
 
         }
     }
